366f2b9464d39e23eeb09e4f883cdb84a51cfe14
Duddikunta Venkata Saieeash Reddy <saieeash@Saieeash-Air.local>
2024-01-30 12:22:48 +0530

    tree problems

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
-
-import Queue.*;
-import java.util.Queue;
+import trees.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        int[] pet = {50,10,60,100};
-        int[] dist = {30,20,100,10};
+        Node root = new Node(20);
 
-        FirstCircularTour.run(pet,dist);
+        root.left = new Node(8);
+        root.right = new Node(12);
+
+        root.left.left = new Node(3);
+        root.left.right = new Node(6);
+
+        root.right.right = new Node(60);
+        root.right.right.left = new Node(70);
+
+        root.right.left = new Node(25);
+//        root.right.right.left.right = new Node(80);
+//
+//        root.right.right.right = new Node(80);
+
+        MaxWidthofBinaryTree.run(root);
 
     }
 
---
+++ src/trees/Node.java
@@ -0,0 +1,12 @@
+package trees;
+
+public class Node {
+
+    public int val;
+    public Node right;
+    public Node left;
+
+    public Node(int val) {
+        this.val = val;
+    }
+}
